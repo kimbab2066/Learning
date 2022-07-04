1f712189f85d47dc2693d0a9dfe1ff210a98c870
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-07-04 16:51:12 +0900

    Stack Queue 활용

---
+++ Learning/src/Java/Chap11/StackAndQueueTest.java
@@ -0,0 +1,18 @@
+package Java.Chap11;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class StackAndQueueTest {
+	static Stack st = new Stack();
+	static Queue que = new LinkedList();
+	static final int MAX_SIZE = 5;
+
+	public static void main(String[] args) {
+		// !input.equals("")와 같지만 null처리를 위해 순서 바꾼 것
+		if (!"".equals("")) {
+
+		}
+	}// end of main
+}// end of class
