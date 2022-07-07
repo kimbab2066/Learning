8c26246e975f01fcfc19176b53d79802410b2ced
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-07-07 14:49:44 +0900

    HashSet Test

---
+++ Learning/src/Java/Chap11/HashSetTest.java
@@ -0,0 +1,10 @@
+package Java.Chap11;
+
+import java.util.HashSet;
+
+public class HashSetTest {
+	public static void main(String[] args) {
+		HashSet<String> set = new HashSet<>(1);
+
+	}// end of main
+}// end of class
